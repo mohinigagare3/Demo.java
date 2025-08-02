13230a1e8b1eb25f0f781e6b4216f6be997e88b2
mohinigagare3 <mohinigagare3@gmail.com>
2025-08-02 20:14:03 +0530

    Add files via upload

---
+++ DefaultConstructor.java
@@ -0,0 +1,9 @@
+public class DefaultConstructor{
+
+DefaultConstructor(){
+System.out.println("Default Constructor is invoked..!!");
+}
+public static void main(String[] args){
+DefaultConstructor obj = new DefaultConstructor();
+}
+}
