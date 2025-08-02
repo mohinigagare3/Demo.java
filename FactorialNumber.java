9aeb7eb448281aff4096cc7ecec90d9225884751
mohinigagare3 <mohinigagare3@gmail.com>
2025-08-02 12:36:59 +0530

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,11 @@
+class FactorialNumber
+{
+public static void main(String[]args)
+{
+int num =5,factorial =1;
+for(int i=1;i<=num;++i){
+factorial*=i;
+}
+System.out.println("Factorialn of"+num+"is:"+factorial);
+}
+}
